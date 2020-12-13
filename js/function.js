e9ee498224c59df99c6b41de05c82ed7b3c1caf8
j-fukushima <64451929+j-fukushima@users.noreply.github.com>
2020-12-13 17:18:36 +0900

    firstcommit

---
+++ js/function.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let elm1 = document.getElementById("1")
+  elm1.addEventListener('click', function() {
+  elm1.innerHTML = "○";
+}, false);
+});
+
+
