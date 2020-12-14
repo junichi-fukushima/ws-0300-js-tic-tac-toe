e28564f65720686a35c8b77e4d712a2be72491b4
j-fukushima <64451929+j-fukushima@users.noreply.github.com>
2020-12-14 16:12:54 +0900

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,8 +1,17 @@
 window.addEventListener("DOMContentLoaded", () => {
-  let elm1 = document.getElementById("1")
-  elm1.addEventListener('click', function() {
-  elm1.innerHTML = "○";
-}, false);
+  let xIsNext = true;
+  document.querySelectorAll(".cell").forEach((element, index) => {
+    element.addEventListener("click", cellClick);
+  });
+  function cellClick(event) {
+    event.target.textContent = xIsNext ? "○" : "×";
+    // status.textContent = "次の手番: " + (xIsNext ? "O" : "X");
+    xIsNext = !xIsNext;
+  }
 });
 
 
+// let elm1 = document.getElementById("1")
+//   elm1.addEventListener('click', function() {
+//   elm1.innerHTML = "○";
+// }, false);
