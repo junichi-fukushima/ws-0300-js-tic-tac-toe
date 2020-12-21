6f1d6e77d953eeac1b710e19f527d743e0e6a720
j-fukushima <64451929+j-fukushima@users.noreply.github.com>
2020-12-22 07:26:03 +0900

    クリック

--- js/function.js
+++ js/function.js
@@ -1,38 +1,50 @@
 window.addEventListener("DOMContentLoaded", () => {
 
   // 各セルの状態を管理する
-  let cells = document.getElementsByClassName("cell")
-  console.log(cells[0].dataset)
-  // let states = [{id:cells[0].dataset.id,state:""},
-  //               {id:cells[1].dataset.id,state:""},
-  //               {id:cells[2].dataset.id,state:""},
-  //               {id:cells[3].dataset.id,state:""},
-  //               {id:cells[4].dataset.id,state:""},
-  //               {id:cells[5].dataset.id,state:""},
-  //               {id:cells[6].dataset.id,state:""},
-  //               {id:cells[7].dataset.id,state:""},
-  //               {id:cells[8].dataset.id,state:""},
-  //               {id:cells[9].dataset.id,state:""},];
+  let cells = document.querySelectorAll(".cell")
+  let states = [{id:cells[0].id,state:""},
+                {id:cells[1].id,state:""},
+                {id:cells[2].id,state:""},
+                {id:cells[3].id,state:""},
+                {id:cells[4].id,state:""},
+                {id:cells[5].id,state:""},
+                {id:cells[6].id,state:""},
+                {id:cells[7].id,state:""},
+                {id:cells[8].id,state:""},];
 
-  // console.log(states[0].id)
+   let NextTurn = true;
+   let turns = document.querySelector(".turn");
 
-  let NextTurn = true;
-  let turns = document.querySelector(".turn");
+   // クリックイベント
+   cells.forEach((element, index) => {
+     element.addEventListener("click", function(){
+       cellClick(element,states,index)
+    },false);
+   });
 
-  // クリックした時の処理を決める関数
-  function cellClick(event) {
-    event.dataset.id = '○';
-    console.log(event)
-    event.target.textContent = NextTurn ? "○" : "×";
-    NextTurn = !NextTurn;
-  }
-
-  // クリックイベント
-  states.forEach((element, index) => {
-    console.log(element)
-    element.addEventListener("click", cellClick(element));
+   // クリックした時の処理を決める関数
+   function cellClick(element,states,index) {
+    if (element.textContent == "") {
+      let target = states[index]
+      target.state = NextTurn ? "○" : "×";
+      element.innerHTML = target.state;
+      if(NextTurn == true){
+        element.classList.toggle('active');
+      }
+    }
     
-  });
+    NextTurn = !NextTurn;
+    
+   }
+
+   // "○" : "×"下線の入れ替え
+   // function TitleSwap(NextTurn){
+   //   if(NextTurn == true){
+   //     element.classList.toggle('apple');
+   //   }else{
+   //    element.classList.toggle('orange');
+   //   }
+   // }
 });
 
 
