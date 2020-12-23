916d966a4d242baeff15b0a890f19f9b970bdf30
j-fukushima <64451929+j-fukushima@users.noreply.github.com>
2020-12-24 08:17:33 +0900

    完成形

--- js/function.js
+++ js/function.js
@@ -2,18 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // 各セルの状態を管理する
   let cells = document.querySelectorAll(".cell")
-  let states = [{id:cells[0].id,state:""},
-                {id:cells[1].id,state:""},
-                {id:cells[2].id,state:""},
-                {id:cells[3].id,state:""},
-                {id:cells[4].id,state:""},
-                {id:cells[5].id,state:""},
-                {id:cells[6].id,state:""},
-                {id:cells[7].id,state:""},
-                {id:cells[8].id,state:""},];
-
-   let NextTurn = true;
-   let turns = document.querySelector(".turn");
+  let states = ["","","","","","","","",""];
 
    // クリックイベント
    cells.forEach((element, index) => {
@@ -22,29 +11,76 @@ window.addEventListener("DOMContentLoaded", () => {
     },false);
    });
 
+   // true/falseで○×を切り替えられるようにする。  
+   let NextTurn = true;
+
    // クリックした時の処理を決める関数
    function cellClick(element,states,index) {
-    if (element.textContent == "") {
-      let target = states[index]
-      target.state = NextTurn ? "○" : "×";
-      element.innerHTML = target.state;
-      if(NextTurn == true){
-        element.classList.toggle('active');
-      }
+    if (calculateWinner(states)) {
+      return;
     }
-    
+    if (element.textContent == "") {
+      states[index] = NextTurn ? "○" : "×";
+      element.innerHTML = states[index];
+      TitleSwap(NextTurn)
+    }
+    calculateWinner(states)
     NextTurn = !NextTurn;
-    
    }
 
-   // "○" : "×"下線の入れ替え
-   // function TitleSwap(NextTurn){
-   //   if(NextTurn == true){
-   //     element.classList.toggle('apple');
-   //   }else{
-   //    element.classList.toggle('orange');
-   //   }
-   // }
+   //  "○" : "×"下線の入れ替え
+   function TitleSwap(NextTurn){
+     let circle = document.getElementById("circle");
+     let cross = document.getElementById("cross");
+     if(NextTurn == true){
+      circle.classList.toggle('active');
+      cross.classList.remove("active");
+     }else{
+      cross.classList.toggle('active');
+      circle.classList.remove("active");
+     }
+   }
+
+   //statusのテキスト(Starting...)を変える  
+   function changestatus(winstate){
+    document.getElementById("status").innerText = `${winstate} win!!`;
+  }
+
+  // RESTARTボタンを押したらstateの状態を初期値にする
+    let restart_btn = document.getElementById("restart_btn")
+    restart_btn.addEventListener("click", function(){
+      states = new Array(9).fill("")
+      cells.forEach((element, index) => {
+      element.innerHTML = states[index]
+      document.getElementById("status").innerText = `starting...`;
+     });
+   },false);
+
+   //勝敗を判定する  
+   function calculateWinner(states) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        states[a] &&
+        states[a] === states[b] &&
+        states[a] === states[c]
+      ) {
+        changestatus(states[a])
+        return states[a];
+      }
+    }
+    return null;
+  }
 });
 
 
